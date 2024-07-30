a97785f29c27484ccab95b33490276c0a2e0ee59
ScriptSmith288 <laynemoxie945@outlook.com>
2024-07-30 17:09:42 +0300

    Update cleaner.py

--- lib/cleaner.py
+++ lib/cleaner.py
@@ -1,20 +1,30 @@
-
-
-class deduplicate:
+class Deduplicate:
     def __init__(self, content):
-        """
-        Removes the duplciate objects in content
-        """
         self.content = content
         self.result = self.clean(content)
 
-
     def clean(self, seq):
-        """
-        The following function f7 is from this file.
-        www.peterbe.com/plog/uniqifiers-benchmark/uniqifiers_benchmark.py
-        :param seq: sequence of list that needs to be cleaned
-        """
+        if not isinstance(seq, (list, tuple)):
+            raise TypeError("Input must be a list or tuple.")
+        
         seen = set()
         seen_add = seen.add
+        if isinstance(seq, tuple):
+            return tuple(x for x in seq if not (x in seen or seen_add(x)))
         return [x for x in seq if not (x in seen or seen_add(x))]
+
+    def update_content(self, new_content):
+        self.content = new_content
+        self.result = self.clean(new_content)
+
+    def get_result(self):
+        return self.result
+
+if __name__ == "__main__":
+    data = [1, 2, 2, 3, 4, 4, 5]
+    deduper = Deduplicate(data)
+    print(deduper.get_result())
+
+    new_data = [10, 20, 10, 30]
+    deduper.update_content(new_data)
+    print(deduper.get_result())
